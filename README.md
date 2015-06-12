9f6ca96cf1d118e70ccb7dcf2af1cb6839b00833
Tomoo Amano <amano@diverse-inc.com>
2015-06-12 10:29:20 +0900

    impl upload form

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # atto
 
-super mini http server
+super mini http server. with upload form
 
 ## Prerequisites
 
@@ -13,6 +13,13 @@ You will need golang.
 ## install
 
     go get me
+
+## Getting Started
+
+   $ atto
+   then, serve current dir files to default port 8080 (option --port is available).
+   If you set params ?upload to url then serve file upload form. and you can upload file
+   to current dir.
 
 ## License
 
--- main.go
+++ main.go
@@ -3,15 +3,72 @@ package main
 import (
 	"flag"
 	"fmt"
+	// _ "github.com/k0kubun/pp"
+	//_ "github.com/motemen/go-loghttp/global"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"path"
 )
 
-var PortNumber *string = flag.String("port", "8080", "port number.")
+var portNumber = flag.String("port", "8080", "port number.")
+
+func uploadForm(w http.ResponseWriter) {
+	fmt.Fprintln(w, `
+<html><head><title>Go upload</title></head>
+<body>
+<form action="./" method="post" enctype="multipart/form-data">
+<label for="file">Filename:</label>
+<input type="file" name="file" id="file">
+<input type="submit" name="submit" value="Submit">
+</form>
+</body>
+</html>
+`)
+}
+
+func uploadFile(w http.ResponseWriter, r *http.Request) error {
+	dir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	targetFile := path.Join(dir, r.URL.Path, header.Filename)
+	out, err := os.Create(targetFile)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, file)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
+	// pp.Println(r)
+	log.Println(r.Method, r.RequestURI)
+	if r.URL.RawQuery == "upload" {
+		uploadForm(w)
+		return
+	}
+	if r.Method == "POST" {
+		err := uploadFile(w, r)
+		if err != nil {
+			fmt.Fprintln(w, err)
+		} else {
+			http.Redirect(w, r, r.URL.Path, 301)
+		}
+		return
+	}
 	dir, err := os.Getwd()
 	if err != nil {
 		fmt.Fprintln(w, err)
@@ -23,5 +80,6 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	flag.Parse()
 	http.HandleFunc("/", rootHandler)
-	log.Fatal(http.ListenAndServe(":"+*PortNumber, nil))
+	log.Println("listen:" + *portNumber)
+	log.Fatal(http.ListenAndServe(":"+*portNumber, nil))
 }
